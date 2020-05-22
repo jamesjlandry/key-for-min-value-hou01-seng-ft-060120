f1df4b0a29970189b9b2696a9816215ef29052bd
James Landry <james.j.landry@outlook.com>
2020-05-22 00:53:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,14 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |kye, val|
     if val < charlie
       charlie = val
-    end
-    if charlie == val
       frank = kye
-    end
-  end
-  
-  name_hash.each do |kye, val|
-    
     end
   end
   frank
